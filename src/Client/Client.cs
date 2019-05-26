89526873323752b6ae2111913922a97398c1c775
Merijn Hendriks <innohurrytocode@gmail.com>
2019-05-26 22:13:10 +0200

    Fix missing Client

---
+++ src/Client/Client.cs
@@ -0,0 +1,25 @@
+﻿using EFTLauncher.Utility;
+
+namespace EFTLauncher.ClientLogic
+{
+    /// <summary>
+    /// Emulated Escape From Tarkov client
+    /// </summary>
+    class Client
+    {
+        ClientLogin clientLogin;
+
+        public void Start(string email, string password)
+        {
+            Logger.Log("INFO: Client starting");
+            clientLogin = new ClientLogin(email, password);
+            clientLogin.Initialize();
+        }
+
+        public void Stop()
+        {
+            clientLogin.Terminate();
+            Logger.Log("INFO: Client terminated");
+        }
+    }
+}
